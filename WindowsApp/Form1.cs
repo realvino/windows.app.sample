f74959787209f8982d77bcbeb667f4c5f262a735
Vinoth <lovelyvino@outlook.com>
2017-02-15 12:11:46 +0530

    sample app
    perform Addition

---
+++ WindowsApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp
+{
+    public partial class Addition : Form
+    {
+        public Addition()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int m = Int32.Parse(textBox1.Text) + Int32.Parse(textBox2.Text);
+            textBox3.Text = m.ToString();
+        }
+
+     
+    }
+}
